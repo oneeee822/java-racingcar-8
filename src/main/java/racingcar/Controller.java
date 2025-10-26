2ebde253059db2941c33241f4fc29685a0d2adf4
OneE <bestoneeee@naver.com>
2025-10-27 01:50:16 +0900

    feat(자동차생성): Car 객체 정의 및 자동차 목록 생성
    Car 클래스를 정의함
    유효성 검사를 통과한 이름 배열로  Car 객체를 생성함
    생성된 Car 객체 목록을 반환하도록 구현함

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -1,17 +1,27 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
 
     public void run(){
         String names = InputView.inputCarNames();
-        setupCars(names);
+        List<Car> cars = setupCars(names);
 
         String tryCnt = InputView.inputTryCnt();
         Validator.validateTryCnt(tryCnt);
     }
 
-    private void setupCars(String carNames){
+    private List<Car> setupCars(String carNames){
         String names[] = carNames.split(",");
         Validator.validateNames(names);
+
+        List<Car> carList = new ArrayList<>();
+        for (String n : names) {
+            carList.add(new Car(n));
+        }
+
+        return carList;
     }
 }
